6e36dde927d93fd6202d6c440b4e94da0fd2caea
Yun Gu <guy36@cardiff.ac.uk>
2022-01-22 12:35:31 +0000

    first version of home page

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,10 +1,48 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div id="nav">
+      <div class="logo">
+        <img src="./assets/yungu.png">
+      </div>
+      <div class="bar">
+          <span class="item" v-for="nav in navs" :key="nav.id">{{nav.text}}</span>
+      </div>
+    </div>
+    
+    <router-view/>
+    
+    <div class="myFooter">
+      <div class="contact">
+        <div class="ways" v-for="(item,i) in footer" :key="i">
+          <h3 class="title">{{item.text}}</h3>
+          <!-- <div class="link"> -->
+            <!-- <img class="icon" src="{{item.icon}}"> -->
+          <p class="link">{{item.link}}</p>
+          <!-- </div> -->
+        </div> 
+      </div>
+      <p id="copyRight">© 2022 YUN GU</p>
+    </div>
   </div>
-  <router-view/>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      navs:[
+        {id:1, text: "Work"},
+        {id:2, text: "Bio"},
+        {id:3, text: "Home"}
+      ],
+      footer:[
+        {text: "Email",link:"GuY36@cardfiff.ac.uk",icon: "@/icon/favicon.ico"},
+        {text: "Instagram",link:"yyyun_k",icon: "@/icon/favicon.ico"}
+      ]
+    }
+  },
+}
+</script>
 
 <style>
 #app {
@@ -16,15 +54,66 @@
 }
 
 #nav {
-  padding: 30px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    height: 80px;
+    background-color: #fff;
+    border-bottom: 1px solid #000;
+    padding-bottom:10px;
 }
 
-#nav a {
+/* #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
+} */
+.logo {
+    width: 20%;
+    /* background-color: #666; */
+    float: left;
+    margin-left: 70px;
+}
+.bar {
+    color: #000;
+    width: 80%;
+    /* background-color: #444; */
+    margin-right: 70px;
+}
+.item {
+  margin: 30px;
+  /* background-color: green; */
+  float: right;
+  font-size:1.5em;
+  font-family: Helvetica, 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.myFooter{
+  width: 100%;
+  /* display: flex;
+  flex-direction: row; */
+  height: 80px;
+  background-color: #fff;
+  border-top: 1px solid #000;
+  padding-top:10px;
+}
+.contact{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+.ways{
+  margin: 0 100px;
+  text-align: left;
+}
+.title{
+  margin: 0
+}
+.link{
+  margin: 0;
+  /* display: flex;
+  flex-direction: row; */
 }
 </style>
--- blog/src/views/Home.vue
+++ blog/src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="photo" class="photo" src="../assets/photo.png">
+    <div class="introduction">
+      <p class="greeting">Hello!</p>
+      <p class="welcome">I am Yun Gu, a postgraduate student @ <a href="https://www.cardiff.ac.uk/computer-science">Cardiff COMSC.</a> Welcome to my website : ).</p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
+
+<style scoped>
+.home{
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
+.photo{
+  width: 100%;
+  display: flex;
+  margin: 30px auto;
+}
+a{
+  cursor: pointer;
+  text-decoration: none;
+}
+a:link{
+  color: rgb(42, 184, 231);
+}
+a:hover{
+  color:aqua;
+}
+a:visited{
+  color:palevioletred
+}
+.introduction{
+  text-align: left;
+}
+.greeting{
+  font-size: 1.5em;
+  font-weight: 600;
+}
+.welcome{
+  font-weight: 500;
+}
+</style>
